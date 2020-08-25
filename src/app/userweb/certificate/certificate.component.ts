647fb74b54726df0d1e1d7ba43c24f74a5171e54
aayush <aayushyadav8105@gmail.com>
2020-08-25 22:32:51 +0530

    home page added

--- src/app/userweb/certificate/certificate.component.ts
+++ src/app/userweb/certificate/certificate.component.ts
@@ -2,10 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { ServerCommService} from "../../shared/services/server-comm.service"
 import {Certificate} from "../../shared/models/certificate"
 import { AuthService} from '../../shared/services/auth.service'
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
 @Component({
   selector: 'app-certificate',
   templateUrl: './certificate.component.html',
-  styleUrls: ['./certificate.component.css']
+  styleUrls: ['./certificate.component.css'],
+  animations: [   trigger('listAnimation', [
+    transition('* => *', [
+
+      query(':enter', style({ opacity: 0 }), {optional: true}),
+
+      query(':enter', stagger('100ms', [
+        animate('1s ease-in', keyframes([
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({opacity: .5, transform: 'translateY(-25px)',  offset: 0.3}),
+          style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+        ]))]), {optional: true})
+    ])
+  ])]
 })
 export class CertificateComponent implements OnInit {
   certificates:Certificate[]
--- src/app/userweb/technews/technews.component.ts
+++ src/app/userweb/technews/technews.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  ServerCommService} from "../../shared/services/server-comm.service";
+import { AuthService } from "../../shared/services/auth.service";
 @Component({
   selector: 'app-technews',
   templateUrl: './technews.component.html',
   styleUrls: ['./technews.component.css']
 })
 export class TechnewsComponent implements OnInit {
+loading:boolean=false
+news
+getdata(){
 
-  constructor() { }
+}
+  constructor(private servComm:ServerCommService,private authserv:AuthService) { }
 
   ngOnInit(): void {
+    this.authserv.waiting.subscribe(res=>{
+      this.loading = res
+    })
+    if(!this.news){
+    this.servComm.getNews().subscribe({
+      next:res=>{
+        if(res){
+          this.news = res
+        }else{
+          this.news = []
+        }
+      }
+    })
+  }
   }
 
 }
